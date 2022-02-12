95c6d609c1c943510447cd2b45de5fc20774a790
vlinux <oldmanpushcart@gmail.com>
2022-02-12 23:13:53 +0800

    开放api包

--- thing-modular/src/main/java/module-info.java
+++ thing-modular/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module io.github.athingx.athing.thing.modular {
+open module io.github.athingx.athing.thing.modular {
     requires io.github.athingx.athing.standard.component;
     exports io.github.athingx.athing.thing.modular;
 }
